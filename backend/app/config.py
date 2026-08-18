a7673a674c36670670bd2d4ef010e37a10a3a7e5
ayush <ayushagarwal.dev@gmail.com>
2026-08-18 18:53:29 +0530

    add: pipecat voice bot functionality

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,13 +6,21 @@ class Settings(BaseSettings):
     MONGO_URL: str
     DB_NAME: str = "live_db"
 
+    # LLM Model
     OPENAI_API_KEY:str
-    EMBEDDING_MODEL:str
+    OPENAI_MODEL:str
 
+    # RAG
+    EMBEDDING_MODEL:str
     CHUNK_SIZE:int
     CHUNK_OVERLAP:int
     VECTOR_INDEX_NAME:str = "vector_index"
     DOCUMENT_CHUNKS_COLLECTION:str= "document_chunks"
+
+    # Voice
+    DEEPGRAM_API_KEY:str
+    CARTESIA_API_KEY:str
+    CARTESIA_VOICE_ID:str
     
     model_config = SettingsConfigDict(
         env_file=".env",
